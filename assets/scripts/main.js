0054d682e9660b26411f0ddf96a542693d997d0d
Rodolphe Souassi <87568171+Rodolphe27@users.noreply.github.com>
2023-10-07 08:14:28 +0200

    Partly design  project page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -40,68 +40,22 @@ document.addEventListener("alpine:init", () => {
         id: "projects",
         title: "Projects",
         content: [
+          
           {
-            id: "mm",
-            title: "Modules manager",
-            github: "",
-            image: "./mm.jpg",
-            description: `
-            Developed collaboratively with a colleague, the Modules Manager is a web-based tool that efficiently manages modules. It provides students with a well-structured table, enabling them to quickly find the starting dates of each module in their curriculum. It simplifies the process of building and publishing a comprehensive module schedule.
-            `,
-          },
-          {
-            id: "creact",
-            title: "Creact",
-            github: "https://github.com/juliush-dev/creact",
+            id: "android-fitness-app",
+            title: "Android_Fitness_App",
+            github: "https://github.com/Rodolphe27/Andriod_Fitness_App",
             image:
-              "https://images.unsplash.com/photo-1633356122544-f134324a6cee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+              "httpsimages.unsplash.comphoto-1554260570-c7068c223285ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1304&q=80",
             description: `
-            This is a collection of React components that I am diligently building as I learn React JS. If you are also learning React, you have the opportunity to clone the repository and delve into the code, gaining insights into the implementation of each component. It's a great way to explore and understand React's capabilities!
-            `,
+            This is a fitness app that I created in Collaboration with four other students for a school project. It is a simple app that allows you to track your daily activities and monitor your progress. It was built using Android Studio and Java.
+            One of the main objective of this project was to learn how to work in a team using the SCRUM framework and gain insights into git and github.       
+                 `,
           },
+          
           {
-            id: "labyrinth",
-            title: "Labyrinth",
-            github: "https://github.com/juliush-dev/labyrinth",
-            image:
-              "https://images.unsplash.com/photo-1636843651345-d59cc9ceef53?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80",
-            description: `
-            A Python-based console game for two players, where the objective is to escape from a labyrinth. Initially, it was created as a gift for my younger brothers.
-            `,
-          },
-          {
-            id: "get-it-for-me",
-            title: "Gifme",
-            github: "https://github.com/juliush-dev/gifme",
-            image:
-              "https://images.unsplash.com/photo-1603566234384-f5f5b59168cc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-            description: `
-            "Or Get It For Me" is an intuitively designed UI-based YouTube video and audio downloader, built on top of the renowned YouTube CLI downloader, youtube-dl.
-            `,
-          },
-          {
-            id: "game-of-life",
-            title: "Game Of Life",
-            github: "https://github.com/juliush-dev/game-of-life",
-            image:
-              "https://images.unsplash.com/photo-1504838986326-0f4d3ce9cf63?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-            description: `
-            A Simple command line based game without player. Game of life is a visualisation of cells living or death, that create special kinds of pattern in each generation.
-            `,
-          },
-          {
-            id: "fx-bounds",
-            title: "FxBounds",
-            github: "https://github.com/juliush-dev/fxBounds",
-            image:
-              "https://images.unsplash.com/photo-1550751827-4bd374c3f58b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-            description: `
-            FxBounds helps is a visual representation of Bounding Rectangles in javafx. It was build to help some of my friends understanding this concept to take intantage of it in their personal projects.
-            `,
-          },
-          {
-            id: "xolve",
-            title: "Xolve",
+            id: "Integral",
+            title: "Integral",
             github: "https://github.com/juliush-dev/xolve",
             image:
               "https://images.unsplash.com/photo-1522542550221-31fd19575a2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
@@ -110,16 +64,7 @@ document.addEventListener("alpine:init", () => {
             `,
           },
 
-          {
-            id: "ritual",
-            title: "Ritual",
-            github: "",
-            image:
-              "https://images.unsplash.com/photo-1542751371-adc38448a05e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-            description: `
-            Currently in the conception phase, Ritual is poised to become a captivating web-based gamified learning platform. It aims to empower individuals to create their own personalized flashcards covering a wide array of topics.
-            `,
-          },
+          
         ],
       },
       {
