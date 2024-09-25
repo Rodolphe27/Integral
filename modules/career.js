75ef540e2aa95496ba2eb71a380461fb959354e9
Rodolphe <87568171+Rodolphe27@users.noreply.github.com>
2024-09-25 12:32:28 +0200

    delete modules file

--- modules/career.js
+++
@@ -1,9 +0,0 @@
-import {setWheelBehaviorFor } from "./utilities/wheelBehavior.js";
-export function careerFunctionalities() {
-    let careerBgWrapper = document.querySelector('#career .section-content .bg-wrapper');
-    let academicBgWrapper = document.querySelector('#academic .section-content .bg-wrapper');
-    let competencesBgWrapper = document.querySelector('#competences .section-content .bg-wrapper');
-    setWheelBehaviorFor(careerBgWrapper);
-    setWheelBehaviorFor(academicBgWrapper);
-    setWheelBehaviorFor(competencesBgWrapper);
-}
